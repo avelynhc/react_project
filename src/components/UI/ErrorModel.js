cab6631071e2d25245e47b35ffbb5f702b6abe93
avelynhc <avelynhc@gmail.com>
2022-03-10 18:22:12 -0500

    changed codes with Portals

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,24 +1,48 @@
-import classes from './ErrorModel.module.css';
+import classes from "./ErrorModel.module.css";
+import ReactDOM from "react-dom";
 
-import Card from './Card';
-import Button from './button';
+import Card from "./Card";
+import Button from "./button";
+import React from "react";
 
-const ErrorModel = props => {
-    return <div>
-        <div className={classes.backdrop} onClick={props.onConfirm} />
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={classes.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={classes.actions}>
-                <Button onClick={props.onConfirm}>Okay</Button>
-            </footer>
-        </Card>
-    </div>
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
 
-}
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModel = (props) => {
+  return (
+    <React.Fragment>
+      {/* // let them know that these should be portal */}
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
+  );
+};
 
 export default ErrorModel;
