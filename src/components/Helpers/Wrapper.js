c3f1287925029736778d080166e172e8bfb83754
avelynhc <avelynhc@gmail.com>
2022-03-10 17:49:22 -0500

    added wrapper and fragment

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,5 @@
+const Wrapper = props => {
+    return props.children; 
+    // children holds all the components you are passing between opening and closing tag of your custom components(Wrapper components)
+}
+export default Wrapper;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/button";
 import ErrorModel from "../UI/ErrorModel";
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -16,20 +17,20 @@ const AddUser = (props) => {
     // validation for user input
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: 'Invalid input', 
-        message: 'Please enter a valid name and age'
-      })
+        title: "Invalid input",
+        message: "Please enter a valid name and age",
+      });
       return;
     }
 
     // +: make sure enteredAge is a number (force conversion)
     if (+enteredAge < 1) {
       setError({
-        title: 'Invalid age', 
-        message: 'Please enter a valid age (>0)'
+        title: "Invalid age",
+        message: "Please enter a valid age (>0)",
       });
       return;
-    };
+    }
 
     props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
@@ -46,11 +47,17 @@ const AddUser = (props) => {
 
   const errorHandler = () => {
     setError(null);
-  }
+  };
 
   return (
-    <div>
-      {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorHandler} />}
+    <Wrapper>
+      {error && (
+        <ErrorModel
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       {/* Card: user defined components, so can't use className used in html built-in components */}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -71,8 +78,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
-
+    </Wrapper>
   );
 };
 
